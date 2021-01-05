fac94e8a1ed3be8e78f68e2db9c4f7e4bcb3c2e7
WeronikaJodkowska <veronica.jodkowska@gmail.com>
2021-01-05 02:54:44 -0800

    CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+    path('create/', views.create, name='create'),
+    path('edit/<int:pk>/', views.edit, name='edit'),
+    path('delete/<int:pk>/', views.delete, name='delete'),
     path('<slug:slug>/', views.detail, name='detail'),
 ]
